649041b251baa33a62ee74c2e54f7241985cdaab
mpls <mpls@posteo.net>
2022-03-27 19:54:08 -0500

    tweak indentation

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ extern crate rand;
 extern crate bip39;
 
 fn main() {
-	
-	 use bip39::{Mnemonic, Language};
-	
-	 let mut rng = rand::thread_rng();
- let m = Mnemonic::generate_in_with(&mut rng, Language::English, 24).unwrap();
+	use bip39::{Mnemonic, Language};
+	let mut rng = rand::thread_rng();
+    let m = Mnemonic::generate_in_with(&mut rng, Language::English, 24).unwrap();
 }
